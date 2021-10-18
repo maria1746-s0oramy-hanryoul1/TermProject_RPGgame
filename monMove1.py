a931de49ce05d49684eaf206bee4aed446138e30
maria1746 <jisu9678@gmail.com>
2021-10-18 15:57:59 +0900

    monMove

--- monMove1.py
+++ monMove1.py
@@ -108,7 +108,7 @@ def redrawGameWindow():
 
 #main loop
 man = player(50, 400, 64, 64)
-goblin = mon(100, 410, 64, 64, 800)
+goblin = mon(100, 410, 64, 64, 750)
 bullets = []
 run = True
 while run:
