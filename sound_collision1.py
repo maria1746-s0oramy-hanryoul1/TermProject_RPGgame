c767d90172b3fdd70011fa08ff18dce3318ff9aa
maria1746 <jisu9678@gmail.com>
2021-11-16 00:23:08 +0900

    사운드 조절

--- sound_collision1.py
+++ sound_collision1.py
@@ -17,6 +17,8 @@ hitSound = pygame.mixer.Sound('sound/hit.mp3')
 
 music = pygame.mixer.music.load('sound/backsound2.mp3')
 pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(.2)
+
 
 score = 0
 
