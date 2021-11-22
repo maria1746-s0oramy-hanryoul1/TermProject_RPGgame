132a78c75c446a5390cd50afc46408ccc5b5e336
maria1746 <jisu9678@gmail.com>
2021-11-22 15:08:14 +0900

    edit

--- stage3.py
+++ stage3.py
@@ -11,7 +11,6 @@ music = pygame.mixer.music.load('sound/backsound3.wav')
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(.2) 
 
-stage3 = pygame.image.load('image/stage3.png')
 bg = pygame.image.load("image/back3.png")
 char = pygame.image.load('image/bl1.png')
 char_life = pygame.image.load('image/heart.png') #캐릭터 목숨 
